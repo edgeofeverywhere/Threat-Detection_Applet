97ab10ecb96773f1625f0c018c553c17d5a35bf4
Edgeofeverywhere <mferrante113@gmail.com>
2024-03-22 16:53:36 -0400

    modified:   app/index.js 	modified:   dist/.DS_Store 	modified:   dist/bundle.js 	modified:   package-lock.json 	modified:   package.json

--- app/index.js
+++ app/index.js
@@ -1,125 +1,139 @@
-import {initJsPsych} from 'jspsych';
+import { initJsPsych } from 'jspsych';
 import 'jspsych/css/jspsych.css';
 import htmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
+import JsPsychPreload from '@jspsych/plugin-preload';
+import JsPsychImageKeyboardResponse from '@jspsych/plugin-image-keyboard-response';
+import 'ndarray';
+import 'ndarray-ops';
+import gaussian from 'gaussian'; 
 
+const Ndarray = require('ndarray');
+const ops = require('ndarray-ops');
 
+function generateNoisyGreyscaleImage(width, height) {
+    const image = new Array(height).fill(null).map(() => new Array(width).fill(0));
 
-// start up the thing and make it scream at you when done
-const jsPsych = initJsPsych({
-  on_finish: function() {
-    jsPsych.data.get().localSave('csv','results.csv');
+    // Generate greyscale image array
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            image[y][x] = Math.floor(Math.random() * 256); // Random value between 0 and 255
+        }
+    }
+
+    // Add noise (Gaussian distribution)
+    const distribution = gaussian(0, 30); // Adjust the standard deviation as needed
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            const noise = Math.round(distribution.ppf(Math.random())); // Gaussian noise
+            image[y][x] = Math.max(0, Math.min(255, image[y][x] + noise)); // Clip the values to 0-255 range
+        }
+    }
+
+    return image;
+}
+
+function imageDataUrl(image) {
+  const width = image[0].length;
+  const height = image.length;
+
+  // Create a canvas element
+  const canvas = document.createElement('canvas');
+  canvas.width = width;
+  canvas.height = height;
+  const context = canvas.getContext('2d');
+
+  // Draw the image onto the canvas
+  const imageData = context.createImageData(width, height);
+  for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+          const value = image[y][x];
+          // Set the color values for each pixel
+          imageData.data[(y * width + x) * 4] = value; // Red channel
+          imageData.data[(y * width + x) * 4 + 1] = value; // Green channel
+          imageData.data[(y * width + x) * 4 + 2] = value; // Blue channel
+          imageData.data[(y * width + x) * 4 + 3] = 255; // Alpha channel (fully opaque)
+      }
   }
-});
+  context.putImageData(imageData, 0, 0);
 
-const subject_id = "Test"
+  // Export the canvas content as a data URL
+  return canvas.toDataURL('image/png');
+}
+
+// Example usage
+const width = 400;
+const height = 300;
+const noisyGreyscaleImage = generateNoisyGreyscaleImage(width, height);
+const imageUrl = imageDataUrl(noisyGreyscaleImage);
+
+const jsPsych = initJsPsych({
+    on_finish: function() {
+        jsPsych.data.get().localSave('csv', 'results.csv');
+    }
+});
 
 const timeline = [];
 
-
-const loader = {
-type: JsPsychPreload,
-auto_preload: true,
-show_progress_bar: true,
-error_message: 'The experiment failed to load. Please refresh the page and try again!'}
-
 const welcome = {
-  type: htmlKeyboardResponse,
-  stimulus: "Welcome to the experiment. Press any key to contin(u)e?!"
+    type: htmlKeyboardResponse,
+    stimulus: "Welcome to the experiment. Press any key to continue!"
 };
 
 const instructions = {
-  type: htmlKeyboardResponse,
-  stimulus: `
-    <p>In this experiment, a circle will appear in the center 
-    of the screen.</p><p>If the circle is <strong>blue</strong>, 
-    press the letter F on the keyboard as fast as you can.</p>
-    <p>If the circle is <strong>orange</strong>, press the letter J 
-    as fast as you can. By ORANGE we mean Yellow, the song by Coldplay.</p>
-    <p>homie we ain't graphic designers you want us to like ensure the placement is chill by fiddlin with tha style? nah b. the youtube thumbnail for BLUE is big and the other one is small</p>
-    <p>you will figure it out even if you can't see the lil one...</p>
-    <div style='width: 100px;'>
-    <div style='float: left;'><img src='img/blue.jpg'></img>
-    <p class='small'><strong>Press the F key</strong></p></div>
-    <div style='float: right;'><img src='img/yellow.jpg'></img>
-    <p class='small'><strong>JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ</strong></p></div>
-    </div>
-    <p>Press any key to begin.</p>
-  `,
-  post_trial_gap: 2000
+    type: htmlKeyboardResponse,
+    stimulus: `
+        <p>In this experiment, a circle will appear in the center 
+        of the screen.</p><p>If the circle is <strong>blue</strong>, 
+        press the letter F on the keyboard as fast as you can.</p>
+        <p>If the circle is <strong>orange</strong>, press the letter J 
+        as fast as you can.</p>
+        <div style='width: 100px;'>
+        <div style='float: left;'><img src='${imageUrl}'></div>
+        <div style='float: right;'><img src='${imageUrl}'></div>
+        </div>
+    `,
+    post_trial_gap: 2000
 };
-
-
-// uncomment it if you want it - we only use bespoke custom images at build time here
-// const preload = {
-  // type: jsPsychPreload,
-  // images: ['img/blue.jpg', 'img/orange.jpg']
-// };
-
-var image_list = [
-  { stimulus: "img/blue.jpg"},
-  { stimulus: "img/yellow.jpg"}
-]
 
 const fixation = {
-  type: htmlKeyboardResponse,
-  stimulus: jsPsych.timelineVariable('stimulus'),
-  choices: ['f', 'j'],
-  data: {
-    task: 'response',
-    correct_response: jsPsych.timelineVariable('correct_response')
-  },
-  on_finish: function(data){
-    data.correct = jsPsych.pluginAPI.compareKeys(data.response, data.correct_response);
-  }
+    type: JsPsychImageKeyboardResponse,
+    stimulus: imageUrl,
+    choices: ['f', 'j'],
+    data: {
+        task: 'response',
+        correct_response: 'f' // Assuming this is the correct response
+    },
+    on_finish: function(data) {
+        data.correct = jsPsych.pluginAPI.compareKeys(data.response, data.correct_response);
+    }
 };
 
-var debrief_block = {
-  type: htmlKeyboardResponse,
-  stimulus: function() {
+const debrief_block = {
+    type: htmlKeyboardResponse,
+    on_start: function () {
+        jsPsych.data.get().localSave('csv', `results.csv`);
+    },
+    stimulus: function() {
+        var trials = jsPsych.data.get().filter({task: 'response'});
+        var correct_trials = trials.filter({correct: true});
+        var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
+        var rt = Math.round(correct_trials.select('rt').mean());
 
-    var trials = jsPsych.data.get().filter({task: 'response'});
-    var correct_trials = trials.filter({correct: true});
-    var accuracy = Math.round(correct_trials.count() / trials.count() * 100);
-    var rt = Math.round(correct_trials.select('rt').mean());
-
-
-    return `<p>You responded correctly on ${accuracy}% of the trials.</p>
-      <p>Your average response time was ${rt}ms.</p>
-      <p>Press any key to complete the experiment.</p>`;
-
-      
-  },
-  on_start: function (){
-    jsPsych.data.get().localSave('json', `219_2024_behavioral_${subject_id}.json`);
-  },
+        return `<p>You responded correctly on ${accuracy}% of the trials.</p>
+            <p>Your average response time was ${rt}ms.</p>
+            <p>Press any key to complete the experiment.</p>`;
+    },
 };
 
-var test = {
-  type: jsPsychImageKeyboardResponse,
-  stimulus: jsPsych.timelineVariable('stimulus'),
-  choices: ['f', 'j'],
-  data: {
-    task: 'response',
-    correct_response: jsPsych.timelineVariable('correct_response')
-  }
-}
-
-var test_procedure = {
-  timeline: [fixation, test],
-  timeline_variables: test_stimuli
-}
-
-var test_procedure = {
-  timeline: [fixation, test],
-  timeline_variables: test_stimuli,
-  randomize_order: true,
-  repetitions: 5
+const test_procedure = {
+    timeline: [fixation],
+    randomize_order: true,
+    repetitions: 1
 };
 
-
-timeline.push(loader);
 timeline.push(welcome);
 timeline.push(instructions);
 timeline.push(test_procedure);
 timeline.push(debrief_block);
-jsPsych.run(timeline)
+
+jsPsych.run(timeline);
