6ccef6de9df22023d822c0dbe3fc83f0d3ae87f3
Edgeofeverywhere <mferrante113@gmail.com>
2024-04-12 13:22:59 -0400

    modified:   app/index.js 	modified:   dist/bundle.js

--- app/index.js
+++ app/index.js
@@ -4,7 +4,6 @@ import './styles/grid.css';
 import htmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
 import 'ndarray';
 import 'ndarray-ops';
-import { Buffer } from 'buffer-es6';
 import gaussian from 'gaussian'; 
 import PreloadPlugin from '@jspsych/plugin-preload';
 
